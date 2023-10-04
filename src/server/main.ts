dc92e14fec93697a58302696d0277ad6fb777847
NovaSagittarii <novasagittarii@gmail.com>
2023-10-04 14:13:20 -0600

    run formatting

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import ViteExpress from "vite-express";
-import { Server } from "socket.io";
-import { createServer } from "http";
+import express from 'express';
+import ViteExpress from 'vite-express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+app.get('/hello', (_, res) => {
+  res.send('Hello Vite + React + TypeScript!');
 });
 
 io.on('connection', (socket) => {
@@ -20,5 +20,5 @@ server.listen(3000, () => {
 });
 
 ViteExpress.bind(app, server, () =>
-  console.log("ViteExpress server is listening on *:3000")
+  console.log('ViteExpress server is listening on *:3000'),
 );
