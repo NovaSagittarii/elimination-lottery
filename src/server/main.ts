91eb5750a098c805a44510bb5213eb6b3a6d3eb2
NovaSagittarii <novasagittarii@gmail.com>
2023-10-04 14:05:45 -0600

    add socket.io to ViteExpress

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('socket.io server listening on *:3000');
+});
+
+ViteExpress.bind(app, server, () =>
+  console.log("ViteExpress server is listening on *:3000")
 );
