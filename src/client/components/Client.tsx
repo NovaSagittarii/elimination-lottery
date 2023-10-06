927e62e9e5d40cb01f163e5e73e7cd609ecf504c
NovaSagittarii <novasagittarii@gmail.com>
2023-10-05 22:02:50 -0600

    add a cool status text

--- src/client/components/Client.tsx
+++ src/client/components/Client.tsx
@@ -5,9 +5,9 @@ import { AppStateContext, ClientStatus, SocketIoClientContext } from '../App';
 import InputText from './InputText';
 
 const STATUS_STYLING = new Map<ClientStatus, string>([
-  ['spectator', 'bg-slate-700'],
-  ['eliminated', 'bg-red-600'],
-  ['candidate', 'bg-green-600'],
+  ['spectator', 'bg-slate-400'],
+  ['eliminated', 'bg-red-300'],
+  ['candidate', 'bg-green-300'],
 ]);
 
 export default function Client() {
@@ -33,10 +33,12 @@ export default function Client() {
       {username && <>you have a username</>}
       <div
         className={
-          'fixed left-0 bottom-0 w-full h-4 overflow-hidden transition-colors duration-300 ' +
+          'fixed left-0 bottom-0 w-full h-6 flex justify-center items-center text-[#0008] text-sm font-extrabold overflow-hidden transition-colors duration-300 ' +
           STATUS_STYLING.get(state.status)
         }
-      ></div>
+      >
+        {state.status.toUpperCase()}
+      </div>
     </div>
   );
 }
